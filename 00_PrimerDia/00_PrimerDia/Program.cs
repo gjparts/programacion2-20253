409eb4634637ae41d3757e2c3a0ed31e646a6cb9
GJParts <39961515+gjparts@users.noreply.github.com>
2025-09-11 11:05:34 -0600

    commit 110920255713

--- 00_PrimerDia/00_PrimerDia/Program.cs
+++ 00_PrimerDia/00_PrimerDia/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _00_PrimerDia
+﻿using System.Threading;
+
+namespace _00_PrimerDia
 {
     internal class Program
     {
@@ -51,6 +53,125 @@
             //Limpiar la pantalla
             Console.Clear();
             Console.WriteLine("Se ha limpiado la pantalla.");
+
+            //Colorear el texto en pantalla
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Texto Amarillo");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Texto Rojo");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Texto Azul");
+
+            //color de fondo y color de texto
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Texto Amarillo con fondo morado.");
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Texto verde con fondo azul oscuro.");
+
+            //reestablecer los colores a su valor predefinido
+            Console.ResetColor();
+
+            //es posible usar varios colores por linea
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Fresa");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Naranja");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Uva");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Sandia");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            //para imprimir un solo String conformado por varios renglones
+            //puede anteponer el caracter @ antes del String, este caracter en C#
+            //permite ignorar los saltos de linea y los caracteres de escape.
+            Console.WriteLine(@"Primera linea
+
+Segunda Linea \
+Tercera Linea \n\n\n\n
+
+\tCuarta Linea
+
+
+Ultima Linea
+");
+            Console.ForegroundColor= ConsoleColor.DarkYellow;
+            Console.WriteLine(@"
+ /\_/\
+( o.o )
+ > ^ <
+");
+            Console.ResetColor();
+
+            //hacer una pausa
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey(true); //true evita que se imprima la tecla presionada
+
+            //Limpiar la pantalla
+            Console.Clear();
+
+            //Establecer la posicion del cursor de impresion en la consola
+            Console.SetCursorPosition(70, 3);
+            Console.WriteLine("Texto en columna 70, linea 3");
+
+            Console.SetCursorPosition(15, 0);
+            Console.WriteLine("Texto en columna 15, linea 0");
+
+            //Importante: el programa continuara imprimiendo a partir
+            //de la ultima linea donde dejo el cursor.
+
+            Console.SetCursorPosition(10, 20);
+            Console.WriteLine("Texto en columna 10, linea 20");
+
+            //hacer una pausa
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey(true); //true evita que se imprima la tecla presionada
+
+            //Limpiar la pantalla
+            Console.Clear();
+
+            //Pausas con tiempo
+            //es necesario importar el namespace llamado System.Threading
+            Console.WriteLine("Haciendo una pausa por 2 segundos...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Listo!");
+
+            Console.WriteLine("Haciendo una pausa por medio segundo...");
+            Thread.Sleep(500);
+            Console.WriteLine("Listo!");
+
+            Console.WriteLine("Haciendo una pausa por 1 segundo...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Listo!");
+
+            //hacer una pausa
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey(true); //true evita que se imprima la tecla presionada
+
+            //Limpiar la pantalla
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            //podemos ir limpiando la pantalla en cada iteracion y simular algo que se mueve
+            for(int i = 0; i < 50; i++)
+            {
+                Console.Clear(); //limpiar pantalla
+
+                Console.SetCursorPosition(i, 10);
+                Console.WriteLine("><>");
+                Console.SetCursorPosition(i + 10, 5);
+                Console.WriteLine("><>");
+                Console.SetCursorPosition(i + 20, 12);
+                Console.WriteLine("><>");
+
+                Thread.Sleep(50); //hacer una pausa de tiempo entre cada iteracion
+            }
+            Console.ResetColor();
         }
     }
 }
--- 00_PrimerDia/00_PrimerDia/obj/Debug/net8.0/00_PrimerDia.AssemblyInfo.cs
+++ 00_PrimerDia/00_PrimerDia/obj/Debug/net8.0/00_PrimerDia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("00_PrimerDia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f83ad0c3688580109955dd409c24f0e5107952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ebf7421c9b383a76934f5279baadf19324a778")]
 [assembly: System.Reflection.AssemblyProductAttribute("00_PrimerDia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("00_PrimerDia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
