7af963853ec7bb806c3927516e018c91f68404dc
gjparts <gjparts@gmail.com>
2025-10-14 10:54:04 -0600

    commit 141020258164

---
+++ 17_random_switch/17_random_switch/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _17_random_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Haga un programa que genere un numero
+            al azar entre 4 y 9.
+            Dependiendo del numero generado debera
+            imprimir el nombre de un color, estos pueden ser:
+            4 = Verde
+            5 = Azul
+            6 = Rojo
+            7 = Gris
+            8 = Amarillo
+            9 = Blanco
+            Evalue el numero al azar utilizando una
+            estructura de seleccion switch.*/
+            Random r = new Random();
+            int n = r.Next(4, 10);
+            switch (n)
+            {
+                case 4: Console.WriteLine("Verde"); break;
+                case 5: Console.WriteLine("Azul"); break;
+                case 6: Console.WriteLine("Rojo"); break;
+                case 7: Console.WriteLine("Gris"); break;
+                case 8: Console.WriteLine("Amarillo"); break;
+                case 9: Console.WriteLine("Blanco"); break;
+            }
+            //el case Default es opcional en C#
+        }
+    }
+}
---
+++ 18_for1/18_for1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _18_for1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) imprimir los numeros del 1 al 10 de uno en uno
+            //10 iteraciones
+            for( int i = 1; i <= 10; i++ )
+                Console.WriteLine(i); //no use llaves porque solo repetire una instruc.
+            //10 iteraciones
+            for (int i = 1; i < 11; i++)
+                Console.WriteLine(i);
+            //10 iteraciones
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i+1);
+
+            Console.WriteLine("******************************");
+            //2) imprimir los numeros del 0 al 50 de dos en dos
+            //26 iteraciones
+            for (int i = 0; i <= 50; i+=2)
+                Console.WriteLine(i);
+            //51 iteraciones
+            Console.WriteLine("******************************");
+            for (int i = 0; i <= 50; i++)
+            {
+                if(i%2 == 0)
+                    Console.WriteLine(i); //imprime i solo si es par
+            }
+            //26 iteraciones
+            Console.WriteLine("******************************");
+            for (int i = 0; i <= 50; i = i+2)
+                Console.WriteLine(i);
+
+            //3) imprimir los numeros del 0 al 50 de cinco en cinco
+            //11 iteraciones
+            Console.WriteLine("******************************");
+            for (int i = 0; i <= 10; i++)
+                Console.WriteLine(i*5);
+            //11 iteraciones
+            Console.WriteLine("******************************");
+            for(int i = 0; i <= 50;i+=5)
+                Console.WriteLine(i);
+
+
+        }
+    }
+}
