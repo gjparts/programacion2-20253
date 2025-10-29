ab981437a05d99d0ba99ee031d0baf30f50ee319
GJParts <39961515+gjparts@users.noreply.github.com>
2025-10-29 10:55:16 -0600

    commit 2910202513521

---
+++ 31_arreglos/31_arreglos/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _31_arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Haga un programa donde esté declarado un arreglo unidimensional de numeros enteros
+            de tamaño 10, en cada uno de los elementos de dicho arreglo
+            deberá colocar un número al azar entre 8 y 76 no importa que se repitan.
+            Por ultimo imprima el contenido de dicho arreglo.*/
+            int[] arreglo = new int[10];
+            Random r = new Random();
+
+            for(int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = r.Next(8,77); //asignar el numero al azar a la i-esima posicion del arreglo
+                Console.WriteLine(arreglo[i]);
+            }
+            /*Cree un nuevo areglo que sea una copia del arreglo anterior; pero
+             en la copia van a modificar cada elemento elevandolo al cubo.
+            Imprimir el contenido del arreglo copia.*/
+            Console.WriteLine("********************************");
+            int[] copia = new int[10];
+            for(int i = 0;i < arreglo.Length; i++)
+            {
+                //copia[i] = arreglo[i]*arreglo[i]*arreglo[i]; //salida mas facil
+                //copia[i] = (int)Math.Pow(arreglo[i], 3); //metodo 1
+
+                //metodo 2: convertir a entero de 32 bit que es lo mismo que int
+                copia[i] = Convert.ToInt32(Math.Pow(arreglo[i], 3));
+                
+                Console.WriteLine(copia[i]);
+            }
+        }
+    }
+}
---
+++ 32_arreglos_foreach/32_arreglos_foreach/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _32_arreglos_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Consideraciones respecto al uso de for-each y arreglos
+            //como vimos anteriormente los arreglos se pueden recorrer con
+            //for tradicional o por medio de for-each.
+            //recuerden que foreach no da acceso al numero de elemento en el arreglo.
+            //lo que deben siempre recordar es que for-each no permite modificar
+            //el elemento en el arreglo, solo se usa para lectura.
+            //en cambio un for tradicional si permite escribir el elemento en el arreglo
+            //ejemplo:
+
+            double[] numeros = { 2.2, 1.5, 4.8, 100, 1.45, 3.1416 };
+            //modifique el arreglo anterior multiplicando por 2 cada elemento.
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = numeros[i] * 2;
+                Console.WriteLine(numeros[i]);
+            }
+
+            Console.WriteLine("*****************************************************");
+            float[] cifras = { 2.2f, 5, 2.5f, 10.1f, 1.1f, 100 };
+            //modifique el arreglo anterior multiplicando por 10 cada elemento.
+            //con for-each no se puede resolver, cada elemento es de solo lectura:
+            /*foreach(float elemento in cifras)
+            {
+                elemento = elemento * 10;
+            }*/
+            //hasta este momento solo lo podemos resolver con for tradicional:
+            for (int i = 0; i < cifras.Length; i++)
+            {
+                cifras[i] = cifras[i] * 10;
+                Console.WriteLine(cifras[i]);
+            }
+        }
+    }
+}
